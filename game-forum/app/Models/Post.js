d00f175275d572bdcf850daf96af8bdfdc116f59
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-09-11 15:14:33 -0600

    delete function fixed

--- game-forum/app/Models/Post.js
+++ game-forum/app/Models/Post.js
@@ -2,24 +2,25 @@ export default class Post{
 
   constructor(data){
     this.title = data.title
-    this.user = data.user
+    this.creatorEmail = data.creatorEmail
     this.category = data.category || "misc"
     this.body = data.body
     this.imgUrl = data.imgUrl || "placehold.it/200x200"
     this.upvotes = data.upvotes || 0
     this.downvotes = data.downvotes || 0
+    this._id = data._id
   }
 
 
   get Template(){
     return `
-    <li onclick="app.postsController.setPost('${this.title}')">${this.category} | ${this.title} | ${this.user} | ${this.upvotes}</li>
+    <li onclick="app.postsController.setPost('${this.title}')">${this.category} | ${this.title} | ${this.creatorEmail} | ${this.upvotes}</li>
     `
   }
 
   get activeTemplate(){
     return `
-    this is the active post :) ${this.title}
+    this is the active post :) ${this.title} <button type="button" class="btn btn-primary" onclick="app.postsController.deletePost('${this._id}')">yeet</button>
     `
   }
 
