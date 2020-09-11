be9c49702ab195b9fe2faf0b6d64c988156a31b5
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-09-11 10:41:13 -0600

    postsService ready

--- server/services/PostsService.js
+++ server/services/PostsService.js
@@ -3,16 +3,35 @@ import { BadRequest } from "../utils/Errors";
 
 class PostsService {
   async find(query = {}) {
-    let posts = await dbContext.Posts.find(query);
-    return posts;
+    let posts = await dbContext.Posts.find(query)
+    return posts
   }
+
   async findById(id) {
-    let post = await dbContext.Posts.findById(id);
+    let post = await dbContext.Posts.findById(id)
     if (!post) {
-      throw new BadRequest("Invalid Id");
+      throw new BadRequest("Invalid Id")
     }
-    return post;
+    return post
+  }
+
+  async create(body) {
+    return await dbContext.Posts.create(body)
+  }
+
+  async edit(body) {
+    let update = dbContext.Posts.findOneAndUpdate({ _id: body.id }, body, { new: true })
+    if (!update) {
+      throw new BadRequest("Invalid id")
+    }
+  }
+
+  async delete(id) {
+    let success = await dbContext.Posts.findByIdAndDelete(id)
+    if (!success) {
+      throw new BadRequest("Invalid id")
+    }
   }
 }
 
-export const postsService = new PostsService();
+export const postsService = new PostsService()
