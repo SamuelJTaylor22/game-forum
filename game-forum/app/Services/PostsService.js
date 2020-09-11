559da4965ab412988f3312494f8577e91458f889
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-09-11 13:56:58 -0600

    added get

--- game-forum/app/Services/PostsService.js
+++ game-forum/app/Services/PostsService.js
@@ -1,13 +1,17 @@
 import { ProxyState } from "../AppState.js";
 import Post from "../Models/Post.js";
+import { api } from "./AxiosService.js";
 
 class PostsService {
+  getPosts() {
+    
+  }
   setPost(id) {
     let foundpost = ProxyState.posts.find(p => p.title == id)
     ProxyState.activePost = foundpost
   }
   addPost(rawPost) {
-
+    api.post(rawPost)
     ProxyState.posts = [...ProxyState.posts, new Post(rawPost)]
   }
 }
