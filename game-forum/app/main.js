2a436c172f09d20030cdce259968db4a9750a725
Darbyblinko <jacobcrombie@live.com>
2020-09-11 16:16:31 -0600

    removed values garbage

--- game-forum/app/main.js
+++ game-forum/app/main.js
@@ -1,10 +1,8 @@
 import { AuthController } from "./Controllers/AuthController.js";
 import PostsController from "./Controllers/PostsController.js";
-import ValuesController from "./Controllers/ValuesController.js";
 
 class App {
   authController = new AuthController();
-  valuesController = new ValuesController();
   postsController = new PostsController();
 }
 
