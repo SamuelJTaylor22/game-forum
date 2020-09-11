08d4d1bf16680fc8789becfd48542a83f93057b8
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-09-11 16:39:21 -0600

    comment stuff

--- game-forum/app/Models/Post.js
+++ game-forum/app/Models/Post.js
@@ -1,6 +1,6 @@
-export default class Post{
+export default class Post {
 
-  constructor(data){
+  constructor(data) {
     this.title = data.title
     this.creatorEmail = data.creatorEmail
     this.category = data.category || "misc"
@@ -12,22 +12,29 @@ export default class Post{
   }
 
 
-  get Template(){
+  get Template() {
     return `
 
     <li class="border border-primary px-2 list-group-item" onclick="app.postsController.setPost('${this.title}')"><h1>${this.category.toUpperCase()} | ${this.creatorEmail} | ${this.title} | <span>${this.upvotes} <i class="fa fa-arrow-up" aria-hidden="true"></i> | ${this.downvotes} <i class="fa fa-arrow-down" aria-hidden="true"></i></span></h1></li>
     `
   }
 
-  get activeTemplate(){
+  get activeTemplate() {
     return `
     <div class="card" style="height: 50%">
                   <div class="card-body">
                     <h4 class="card-title">${this.title} | ${this.creatorEmail}</h4>
                     <img class="card-img" src="${this.imgUrl}" alt="" style="">
                     <p class="card-text">${this.body}</p>
-<button type="button" class="btn btn-primary" onclick="app.postsController.deletePost('${this._id}')">yeet</button>
-                  </div>
+                    <button type="button" class="btn btn-primary" onclick="app.postsController.deletePost('${this._id}')">yeet</button>
+                    <form class="form-inline my-1" onsubmit="app.commentsController.addComment(event)">
+          <div class="form-group">
+            <input type="text" name="comment" id="comment" class="ml-1 form-control" placeholder="Leave comment"
+              aria-describedby="helpId">
+          </div>
+          <i class="fa fa-plus-circle" aria-hidden="true"></i><button type="submit" class="btn btn-warning form-control">Comment!</button>
+        </form>
+                </div>
     </div>
     `
   }
