0d0b803e4d6b1b103830a5666b88381edaa7b6cb
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-09-11 14:20:35 -0600

    misc fixes, including user and api changes

--- game-forum/app/Services/PostsService.js
+++ game-forum/app/Services/PostsService.js
@@ -4,14 +4,16 @@ import { api } from "./AxiosService.js";
 
 class PostsService {
   getPosts() {
-    
+    let res = api.get("api/posts")
+    ProxyState.posts = res.map(p => new Post(p))
   }
   setPost(id) {
     let foundpost = ProxyState.posts.find(p => p.title == id)
     ProxyState.activePost = foundpost
   }
   addPost(rawPost) {
-    api.post(rawPost)
+    rawPost.user = ProxyState.user.email
+    api.post("posts",rawPost)
     ProxyState.posts = [...ProxyState.posts, new Post(rawPost)]
   }
 }
