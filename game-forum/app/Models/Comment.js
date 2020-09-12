bd43a1a4bd8cbda87788bd8ea5ac42b4873152c6
Ben W <bwiedeback@gmail.com>
2020-09-11 20:01:39 -0600

    comment template updated, model uses creator email

--- game-forum/app/Models/Comment.js
+++ game-forum/app/Models/Comment.js
@@ -2,7 +2,7 @@ export default class Comment {
 
   constructor(data) {
     this.post = data.post
-    this.user = data.user
+    this.creatorEmail = data.creatorEmail
     this.body = data.body
     this.upvotes = data.upvotes || 0
     this.downvotes = data.downvotes || 0
@@ -10,7 +10,7 @@ export default class Comment {
 
   get Template() {
     return `
-      <h1>${this.body}</h1>
+      <h1 class="border border-primary">${this.creatorEmail} | ${this.body} | <span>${this.upvotes} <i class="fa fa-arrow-up" aria-hidden="true"></i>  ${this.downvotes} <i class="fa fa-arrow-down" aria-hidden="true"></i></span></h1>
     `
   }
 
