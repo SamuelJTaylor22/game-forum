8083160981efab64686978458be6fc2fe4520ab3
Kyle Bennion <bilekennion@hotmail.com>
2020-09-12 01:55:55 -0600

    Custom Font Complete Delete Symbol added

--- client/app/Models/Comment.js
+++ client/app/Models/Comment.js
@@ -12,7 +12,7 @@ export default class Comment {
   get Template() {
     return `
     <div class="card mt-1 border border-primary">
-    <h2 class="my-1 ml-1">${this.creatorEmail} | ${this.body} | <span>${this.upvote.length} <i class="fa fa-arrow-up" aria-hidden="true" onclick="app.commentsController.vote(true, '${this._id}')"></i>  ${this.downvote.length} <i class="fa fa-arrow-down" aria-hidden="true" onclick="app.commentsController.vote(false, '${this._id}')"></i></span><button type="button" class="btn btn-syellow float-right mr-1" onclick="app.commentsController.deleteComment('${this._id}')">Delete Comment</button></h2>
+    <h2 class="my-1 ml-1">${this.creatorEmail} | ${this.body} | <span>${this.upvote.length} <i class="fa fa-arrow-up" aria-hidden="true" onclick="app.commentsController.vote(true, '${this._id}')"></i>  ${this.downvote.length} <i class="fa fa-arrow-down" aria-hidden="true" onclick="app.commentsController.vote(false, '${this._id}')"></i></span><button type="button" class="btn btn-syellow float-right mr-1 stext" onclick="app.commentsController.deleteComment('${this._id}')">Delete Comment  <i class="fa fa-minus-circle" aria-hidden="true"></i>  </button></h2>
 </div>
     `
   }
